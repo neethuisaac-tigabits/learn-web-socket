cc5d42c40fab53c0fcd3b0e3197c80bc31d49ece
Neethu Isaac <neethu.isaac@tigabits.com>
2026-05-07 15:41:36 +0530

    Order API with sanctum middleware

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,37 +10,61 @@ use Illuminate\Support\Facades\Log;
 
 class OrderController extends Controller
 {
-    public function index() {
-        $orders = Order::all();
+    public function index(Request $request) {
+
+        $orders = Order::where('user_id', $request->user()->id)->get();
+
+        if($request->is('api*')) {
+            return response()->json($orders, 200);
+        }
+        
         return view('orders', compact('orders'));
 
     }
     public function update(Request $request, $id) {
+
         $order = Order::find($id);
         $order->amount += 1;
         $order->save();
 
         OrderChanged::dispatch($order);
 
-        return "done {$order->amount}";
+        if($request->is('api*')) {
+            return response()->json($order, 200);
+        }
+        else {
+            return "done {$order->amount}";
+        }
     }
     public function show (Request $request, $id) {
-        Log::info("id passed in order show request " . $id  . " and path : " . request()->fullUrl());
-        $order = Order::find($id);
 
-        return view('order',  compact('order'));
+        $order = Order::where('id', $id)->where('user_id', $request->user()->id)->first();
+
+        if($request->is('api*')) {
+            return response()->json($order, 200);
+        }
+        else {
+            return view('order',  compact('order'));
+        }
     }   
     public function store(Request $request) {
+
         $input = $request->validate([
             'name' => ['required'],
             'amount' => ['required', 'int'],
             'bill_no' => ['required'],
         ]);
+
         $input['user_id'] = Auth::id();
         $order = Order::create($input);
-        if(!empty($order)) {
-            return redirect("/orders/{$order->id}");
+        if($request->is('api*')) {
+            return response()->json($order, 200);
         }
-        return back();
+        else {
+            if(!empty($order)) {
+                return redirect("/orders/{$order->id}");
+            }
+            return back();
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiLoginController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('/login', [ApiLoginController::class, 'authenticate']);
+Route::middleware('auth:sanctum')->prefix('orders')->group(function() {
+    Route::get('/', [OrderController::class, 'index']);
+    Route::post('/', [OrderController::class, 'store']);
+    Route::put('/{id}', [OrderController::class, 'update'])->where(['id' => '[0-9]+']);
+    Route::get('/{id}', [OrderController::class, 'show'])->where(['id' => '[0-9]+']);
+});
