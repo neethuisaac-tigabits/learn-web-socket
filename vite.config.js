4b4fb4f74d45d5728f7c202d06d3d3d96b8c1314
Neethu Isaac <neethu.isaac@tigabits.com>
2026-05-07 12:10:05 +0530

    Vite error fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/bootstrap.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/bootstrap.js',  'resources/js/orders.js'],
             refresh: true,
         }),
         tailwindcss(),
